7b604e2148c04b4fdddddf61ea771948e69ebe1d
Emizy <adenitiree@gmail.com>
2017-12-10 10:27:55 +0100

    Second

--- NevoGas/settings.py
+++ NevoGas/settings.py
@@ -128,12 +128,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+# #offline setting
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#
+# MEDIA_URL = '/media/'
 
+#online setting
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,  'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
